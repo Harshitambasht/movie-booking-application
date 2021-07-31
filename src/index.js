2b6d3270430bca30626fd1651e77fc22b1944ba9
Harshit Ambasht <harshit.ambasht@gmail.com>
2021-08-01 00:58:52 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './common/header/Header';
+import Home from './screens/home/Home';
+import {GridList1} from './screens/home/Home';
 ReactDOM.render(
   <div>
-    <Header/>
+    <Home/>
+    <GridList1/>
   </div>,
   document.getElementById('root')
 );
