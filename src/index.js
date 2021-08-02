9af41db8f144461b8bbc8f61d3d4ed949b35117f
Harshit Ambasht <harshit.ambasht@gmail.com>
2021-08-02 14:53:32 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './screens/home/Home';
+import Home, { Left, OutlinedCard } from './screens/home/Home';
 import {GridList1} from './screens/home/Home';
 ReactDOM.render(
   <div>
     <Home/>
     <GridList1/>
+    <div className="flex-container">
+      <div className="left">
+    <Left/>
+    </div>
+    <div className="right">
+    <OutlinedCard/>
+    </div>
+    </div>
   </div>,
   document.getElementById('root')
 );
